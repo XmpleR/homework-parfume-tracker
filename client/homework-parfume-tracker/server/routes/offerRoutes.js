c5511389bb7f8c98abd69ffbcf4e94ba56823ed8
XmpleR <rishko2003@gmail.com>
2025-06-19 00:39:07 +0200

    Delete unwanted nested client/homework-parfume-tracker folder

--- client/homework-parfume-tracker/server/routes/offerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getOffers, createOffer } = require('../controllers/offerController');
-
-router.get('/', getOffers);
-router.post('/', createOffer);
-
-module.exports = router;
